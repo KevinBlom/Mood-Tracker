d7b1faef864c98eae02b47f68d6e820d60af17fa
Kevin Blom <>
2020-06-21 21:00:52 +0200

    Commit

--- MoodTracker/Views/Settings.swift
+++ MoodTracker/Views/Settings.swift
@@ -9,12 +9,41 @@
 import Foundation
 import SwiftUI
 import UIKit
+import CoreData
+
 
 struct Settings: View {
+    
+    @Environment(\.managedObjectContext) var managedObjectContext: NSManagedObjectContext
 
     var body: some View {
+        
+        
         List {
             DeleteAllButton()
+            
         }.navigationBarTitle("Settings")
+            .onAppear {
+                let mood = MoodEntity(context: self.managedObjectContext)
+                mood.id = UUID()
+                mood.date = Date()
+                // print("Submitting moodID: 3")
+                mood.moodID = Int32(3)
+                
+                let summary = DaySummaryEntity(context: self.managedObjectContext)
+                summary.id = UUID()
+                summary.date = Date()
+                summary.numberOfMoods += 1
+                summary.averageMood = mood.moodID
+                mood.summary = summary
+                print("On appear")
+                
+                do {
+                    try self.managedObjectContext.save()
+                } catch let error {
+                    print("This is the error:\(error)")
+                    // handle the Core Data error
+                }
+        }
     }
 }
